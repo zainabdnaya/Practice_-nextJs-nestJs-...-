707933c2d769518001cb2693af9a99cbf3c6aaf5
zdnaya <zdnaya@student.1337.ma>
2022-02-12 12:40:35 +0100

    2022-02-12 12:40:35

--- react/my-app/src/App.tsx
+++ react/my-app/src/App.tsx
@@ -1,140 +1,65 @@
-
 import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
-// import { useRef, useEffect } from 'react'
 import { Socket, io } from 'socket.io-client';
 import "./index.css";
-// import { Game } from './Game';
 import Canvas from './Game';
 import { useState } from 'react';
 
+import { Button, View, StyleSheet } from 'react-native';
 
 
-type FormValues = {
-  firstName: string;
-  lastName: string;
-  email: string;
-};
+// export default class GridView extends Component {
+
+//   render() {
+//        return (
+//            <View style={styles.container}>
+//              <View style={styles.buttonContainer}>
+//                <Button title="Button 1"/>
+//              </View>
+//              <View style={styles.buttonContainer}>
+//                <Button title="Button 2"/>
+//              </View>
+//            </View>
+//        );
+//    }
+// }
+
+const styles = StyleSheet.create({
+  backgroundVideo: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0
+  },
+
+  buttonStyleContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    marginHorizontal: 20,
+    marginTop: 5,
+  }
+});
+// export default VideoPlayer;
 
 
 export default function App() {
 
-
-
-  const { register, handleSubmit } = useForm<FormValues>();
-  const [showCanvas, setShowCanvas] = useState(false);
-  const [showForm, setShowForm] = useState(true);
-  const [showTtile, setTtile] = useState(true);
-  const [CanvasTitle, setCanvasTitle] = useState(false);
-  const [datas, setData] =  useState({Id:"hello", Name:"hello", Email:""});    
-  let T 
-  if(window.sessionStorage.getItem("Email")!=null)
-  {
+    return (
+      <div className="App">
     
-  return (
-    
-    <div  className="App">
-      { <Canvas data={datas.valueOf()} />}
-    </div>
+    <View style={styles.container}>
+             <View style={styles.buttonContainer}>
+               <Button title="Play"/>
+             </View>
+             <View style={styles.buttonContainer}>
+               <Button title="Watch"/>
+             </View>
+           </View>
+      </div>
 
-  );
-  }
-  //initial WebSocketServer 
-  const socket = io('http://localhost:3080');
-  
-  const onSubmit: SubmitHandler<FormValues> = (data) => {
-    // alert(JSON.stringify(data));
-    
-    setData({Id:data.firstName, Name:data.lastName,Email:data.email});
-    setShowForm(false);
-    setShowCanvas(true);
-    setTtile(false);
-    setCanvasTitle(true);
-    window.sessionStorage.setItem("Email",data.email);
-    socket.emit('msgToServer', data);
-  }
+    );
 
-  return (
-    
-    <div  className="App">
-      {/* <center> */}
-      {showTtile
-        && <h1>Ping Pong Player - Registration</h1>
-      }
-      {CanvasTitle && <h1>Ping Pong Player - Canvas</h1>}
-      {showForm &&
-        <form onSubmit={handleSubmit(onSubmit)} noValidate>
-          <div>
-            <label>First Name</label>
-            <input {...register("firstName")} />
-          </div>
-
-          <div>
-            <label>Last Name</label>
-            <input {...register("lastName")} />
-          </div>
-
-          <div>
-            <label>Email</label>
-            <input {...register("email")} type="email" />
-          </div>
-
-          <input type="submit" />
-        </form>
-      }
-      {/* {CanvasTitle && <h1>Ping Pong Player - Canvas</h1>} */}
-      
-
-
-      {/* {} */}
-      {showCanvas && <Canvas data={datas.valueOf()} />}
-
-    {/* </center> */}
-    </div>
-
-  );
 }
 
 
-// import React from "react";
-// import { useForm, SubmitHandler } from "react-hook-form";
-// // import { useRef, useEffect } from 'react'
-// // import { Socket, io } from 'socket.io-client';
-// import "./index.css";
-// // import {Game} from './Game';
-// import Canvas from './Game';
-
-// type FormValues = {
-//   firstName: string;
-//   lastName: string;
-//   email: string;
-// };
-
-// export default function App() {
-//   const { register, handleSubmit } = useForm<FormValues>();
-
-
-//   return (
-//     <div className="App">
-//       <h1>Ping Pong Player - Registration</h1>
-//       <form onSubmit={handleSubmit(onSubmit)} noValidate>
-//         <div>
-//           <label>First Name</label>
-//           <input {...register("firstName")} />
-//         </div>
-
-//         <div>
-//           <label>Last Name</label>
-//           <input {...register("lastName")} />
-//         </div>
-
-//         <div>
-//           <label>Email</label>
-//           <input {...register("email")} type="email" />
-//         </div>
-
-//         <input type="submit" />
-//       </form>
-//     </div>
-//   );
-// }
